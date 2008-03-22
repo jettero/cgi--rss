3ab40f9165f04413ae2cae091977841dbb803d35
Paul Miller <jettero@voltar.org>
2008-03-22 12:02:27 -0400

    pause fails on https protocol... pfft.

--- RSS.pm
+++ RSS.pm
@@ -4,7 +4,7 @@ package CGI::RSS;
 use strict;
 use base 'CGI';
 
-our $VERSION = '0.7.0';
+our $VERSION = '0.7.1';
 
 # TODO: this collection of tag names is hardly "correct" or complete
 our @TAGS = qw(
